d32e98a14222d1af19cbb37eee6e42e9a07f5cd9
ptriantafyll <ptriantafyll@gmail.com>
2025-02-09 15:09:09 +0200

    Added clickable categories in grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_internals/categories.dart';
+import 'package:meals_app/screens/categories.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
