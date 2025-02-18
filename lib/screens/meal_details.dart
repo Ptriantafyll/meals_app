7ae2a3be55f5735ee6fde425930893e52b2e6c6e
ptriantafyll <ptriantafyll@gmail.com>
2025-02-18 18:32:50 +0200

    Added provider for favorite meals

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 // import 'package:transparent_image/transparent_image.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
 
-class MealDetailsScreen extends StatelessWidget {
+class MealDetailsScreen extends ConsumerWidget {
   const MealDetailsScreen({
     super.key,
     required this.meal,
-    required this.onToggleFavorite,
   });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavorite;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     // Widget content = FadeInImage(
     //   placeholder: MemoryImage(kTransparentImage),
     //   image: NetworkImage(meal.imageUrl),
@@ -28,7 +28,18 @@ class MealDetailsScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              onToggleFavorite(meal);
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(wasAdded
+                      ? 'Meal added as a favorite'
+                      : 'Meal removed from favorites'),
+                ),
+              );
             },
             icon: const Icon(Icons.star),
           )
